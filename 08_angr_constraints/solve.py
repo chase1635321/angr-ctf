9dca288100d4a188c21aef1034db734a2ff76e91
Chase Kanipe crkanip <crkanip@ip-10-113-155-69.evoforge.org>
2020-02-03 16:44:38 +0000

    Automatic push

---
+++ 08_angr_constraints/solve.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import angr
+import claripy
+
+project = angr.Project("08_angr_constraints")
+state = project.factory.blank_state(addr=0x804862a)
+simgr = project.factory.simgr(state)
+
+password = claripy.BVS("password", 8*16)
+
+state.memory.store(0x0804a050, password)
+
+simgr.explore(find=0x08048678)
+
+if simgr.found:
+    solution = simgr.found[0]
+    solution_BVS = solution.memory.load(0x804a050, 16)
+    solution.add_constraints(solution_BVS == "BWYRUBQCMVSBRGFU")
+    s = solution.se.eval(password, cast_to=bytes).decode()
+    print(s)
+else:
+    print("No solution found")
